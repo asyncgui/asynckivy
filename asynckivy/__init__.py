4e0756bfc6784ba692123869b855a89d83b5afd3
gottadiveintopython <flow4re2c@gmail.com>
2019-12-09 00:57:20 +0900

    'gather', 'and_' and 'or_' are now official components

--- asynckivy/__init__.py
+++ asynckivy/__init__.py
@@ -1,7 +1,8 @@
 __version__ = '0.0.2'
-__all__ = ('start', 'sleep', 'event', 'thread', )
+__all__ = ('start', 'sleep', 'event', 'thread', 'gather', 'and_', 'or_', )
 
 import types
+import typing
 from functools import partial
 from kivy.clock import Clock
 from inspect import getcoroutinestate, CORO_CLOSED
@@ -63,3 +64,48 @@ async def thread(func, *args, **kwargs):
     while not is_finished:
         await sleep(3)
     return return_value
+
+
+class Task:
+    __slots__ = ('coro', 'done', 'result', 'done_callback')
+    def __init__(self, coro, *, done_callback=None):
+        self.coro = coro
+        self.done = False
+        self.result = None
+        self.done_callback = done_callback
+    async def _run(self):
+        self.result = await self.coro
+        self.done = True
+        if self.done_callback is not None:
+            self.done_callback()
+
+
+@types.coroutine
+def gather(coros:typing.Iterable[typing.Coroutine], *, n:int=None) -> typing.Sequence[Task]:
+    coros = tuple(coros)
+    n_coros_left = n if n is not None else len(coros)
+    step_coro = None
+
+    def done_callback():
+        nonlocal n_coros_left
+        n_coros_left -= 1
+        if n_coros_left == 0:
+            step_coro()
+    tasks = tuple(Task(coro, done_callback=done_callback) for coro in coros)
+    for task in tasks:
+        start(task._run())
+
+    def callback(step_coro_):
+        nonlocal step_coro
+        step_coro = step_coro_
+    yield callback
+
+    return tasks
+
+
+async def or_(*coros):
+    return await gather(coros, n=1)
+
+
+async def and_(*coros):
+    return await gather(coros)
--- examples/wait_for_multiple_coroutines_to_complete.py
+++ examples/wait_for_multiple_coroutines_to_complete.py
@@ -1,52 +1,4 @@
-import types
-import typing
 import asynckivy as ak
-
-
-class Task:
-    __slots__ = ('coro', 'done', 'result', '_done_callback')
-    def __init__(self, coro, *, done_callback=None):
-        self.coro = coro
-        self.done = False
-        self.result = None
-        self._done_callback = done_callback
-    async def _run(self):
-        self.result = await self.coro
-        self.done = True
-        if self._done_callback is not None:
-            self._done_callback()
-
-
-@types.coroutine
-def gather(coros:typing.Iterable[typing.Coroutine], *, n:int=None) -> typing.Sequence[Task]:
-    coros = tuple(coros)
-    n_coros_left = n if n is not None else len(coros)
-    step_coro = None
-
-    def done_callback():
-        nonlocal n_coros_left
-        n_coros_left -= 1
-        if n_coros_left == 0:
-            step_coro()
-    tasks = tuple(Task(coro, done_callback=done_callback) for coro in coros)
-    for task in tasks:
-        ak.start(task._run())
-
-    def callback(step_coro_):
-        nonlocal step_coro
-        step_coro = step_coro_
-    yield callback
-
-    return tasks
-
-
-async def or_(*coros):
-    return await gather(coros, n=1)
-
-
-async def and_(*coros):
-    return await gather(coros)
-
 
 
 def _test():
@@ -91,12 +43,12 @@ def _test():
                 # wait for the completion of any number of coroutines
                 # ----------------------------------------------------
                 label.text = f'wait until 2 of {len(buttons)} buttons are pressed'
-                tasks = await gather(
+                tasks = await ak.gather(
                     (ak.event(button, 'on_press') for button in buttons),
                     n=2
                 )
                 label.text = 'Done! ({} and {} were pressed)'.format(
-                    *[task.result[0][0].text for task in tasks if task.done]
+                    *[task.result[0].text for task in tasks if task.done]
                 )
                 await ak.sleep(1)
                 label.text = 'next'
@@ -106,7 +58,7 @@ def _test():
                 # wait for the completion of one coroutine 
                 # -----------------------------------------
                 label.text = "wait until button'A' is pressed or 5s passes"
-                tasks = await or_(
+                tasks = await ak.or_(
                     ak.event(buttons[0], 'on_press'),
                     ak.sleep(5),
                 )
@@ -121,7 +73,7 @@ def _test():
                 # wait for the completion of all coroutines
                 # ------------------------------------------
                 label.text = "wait until all buttons are pressed and 5s passes"
-                tasks = await and_(
+                tasks = await ak.and_(
                     ak.sleep(5),
                     *(ak.event(button, 'on_press') for button in buttons)
                 )
