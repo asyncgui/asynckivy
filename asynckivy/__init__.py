e75597be228b0d52f07e61d111e6b5d8f458564d
gottadiveintopython <flow4re2c@gmail.com>
2019-12-11 15:10:10 +0900

    add 'animation'

--- asynckivy/__init__.py
+++ asynckivy/__init__.py
@@ -1,5 +1,8 @@
 __version__ = '0.0.2'
-__all__ = ('start', 'sleep', 'event', 'thread', 'gather', 'and_', 'or_', )
+__all__ = (
+    'start', 'sleep', 'event', 'thread', 'gather', 'and_', 'or_',
+    'animation',
+)
 
 import types
 import typing
@@ -109,3 +112,75 @@ async def or_(*coros):
 
 async def and_(*coros):
     return await gather(coros)
+
+
+async def animation(target, **kwargs):
+    from time import perf_counter as get_current_time
+    from kivy.animation import AnimationTransition
+
+    duration = kwargs.pop('d', kwargs.pop('duration', 1.))
+    transition = kwargs.pop('t', kwargs.pop('transition', 'linear'))
+    step = kwargs.pop('s', kwargs.pop('step', 0))
+    if isinstance(transition, str):
+        transition = getattr(AnimationTransition, transition)
+    animated_properties = kwargs
+
+    # get current values
+    properties = {}
+    for key, value in animated_properties.items():
+        original_value = getattr(target, key)
+        if isinstance(original_value, (tuple, list)):
+            original_value = original_value[:]
+        elif isinstance(original_value, dict):
+            original_value = original_value.copy()
+        properties[key] = (original_value, value)
+
+    # assigning to a local variable might improve the performance
+    calculate_ = _animation_calculate
+    sleep_ = sleep
+
+    if not duration:
+        await sleep_(0)
+        for key, values in properties.items():
+            a, b = values
+            setattr(target, key, b)
+        return
+
+    start_time = get_current_time()
+    while True:
+        await sleep_(step)
+
+        # calculate progression
+        progress = min(1., (get_current_time() - start_time) / duration)
+        t = transition(progress)
+
+        # apply progression on target
+        for key, values in properties.items():
+            a, b = values
+            value = calculate_(a, b, t)
+            setattr(target, key, value)
+
+        # time to stop ?
+        if progress >= 1.:
+            return
+
+
+def _animation_calculate(a, b, t):
+    if isinstance(a, list) or isinstance(a, tuple):
+        if isinstance(a, list):
+            tp = list
+        else:
+            tp = tuple
+        return tp([_animation_calculate(a[x], b[x], t) for x in range(len(a))])
+    elif isinstance(a, dict):
+        d = {}
+        for x in iterkeys(a):
+            if x not in b:
+                # User requested to animate only part of the dict.
+                # Copy the rest
+                d[x] = a[x]
+            else:
+                d[x] = _animation_calculate(a[x], b[x], t)
+        return d
+    else:
+        return (a * (1. - t)) + (b * t)
---
+++ examples/loop_animation2.py
@@ -0,0 +1,48 @@
+from kivy.app import App
+from kivy.lang import Builder
+import asynckivy as ak
+
+
+KV_CODE = r'''
+Widget:
+    Label:
+        id: label
+        font_size: 60.0
+        size: self.texture_size
+'''
+
+
+class TestApp(App):
+    def build(self):
+        return Builder.load_string(KV_CODE)
+
+    def on_start(self):
+        ak.start(animate(self.root))
+
+
+async def animate(root):
+    from asynckivy import animation as a
+    l = root.ids.label
+    async def _blink_forever():
+        while True:
+            await a(l, color=(0, .3, 0, 1, ), t='out_quad')
+            await a(l, color=(1, 1, 1, 1, ), t='in_quad')
+    async def _round():
+        await a(l, right=root.right)
+        await a(l, top=root.top)
+        await a(l, x=root.x)
+        await a(l, y=root.y)
+    ak.start(_blink_forever())
+    while True:
+        l.text = 'Hello'
+        await _round()
+        await a(l, center=root.center)
+        await a(l, font_size=100.0, s=.1)
+        l.text = 'Kivy'
+        await a(l, pos=root.pos)
+        await _round()
+        await a(l, font_size=60.0, s=.1)
+
+
+if __name__ == '__main__':
+    TestApp().run()
