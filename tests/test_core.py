0e8fe4178b465db41b311d2da8dae2943a8df9c5
gottadiveintopython <flow4re2c@gmail.com>
2020-01-28 18:58:51 +0900

    add more tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -35,6 +35,37 @@ def test_gather(ed_cls):
     assert done
 
 
+def test_gather2(ed_cls):
+    import time
+    from kivy.clock import Clock
+    import asynckivy as ak
+    eds = [ed_cls() for __ in range(2)]
+    async def _test():
+        tasks = await ak.gather(
+            [
+                *(ak.event(ed, 'on_test') for ed in eds),
+                ak.sleep(.1),
+            ],
+            n=2,
+        )
+        assert tasks[0].done
+        assert not tasks[1].done
+        assert tasks[2].done
+        nonlocal done
+        done = True
+    Clock.tick()
+    done = False
+    ak.start(_test())
+    assert not done
+    eds[0].dispatch('on_test')
+    assert not done
+    eds[0].dispatch('on_test')
+    assert not done
+    time.sleep(.15)
+    Clock.tick()
+    assert done
+
+
 def test_or_(ed_cls):
     import asynckivy as ak
     eds = [ed_cls() for __ in range(3)]
---
+++ tests/test_sleep.py
@@ -0,0 +1,20 @@
+def test_sleep():
+    import time
+    from kivy.clock import Clock
+    import asynckivy as ak
+    async def _test():
+        await ak.sleep(.1)
+        nonlocal done
+        done = True
+    done = False
+    Clock.tick()
+    ak.start(_test())
+    assert not done
+    Clock.tick()
+    assert not done
+    time.sleep(.07)
+    Clock.tick()
+    assert not done
+    time.sleep(.07)
+    Clock.tick()
+    assert done
