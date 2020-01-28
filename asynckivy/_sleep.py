b8f3bad7a04896945e05f3d3223253974fcc38fa
gottadiveintopython <flow4re2c@gmail.com>
2020-01-28 19:09:10 +0900

    wrapping in 'functools.partial()' is not necessary
    It was necessary for 'callbackgoaway', which is the predecessor of this project

--- asynckivy/_sleep.py
+++ asynckivy/_sleep.py
@@ -1,7 +1,6 @@
 __all__ = ('sleep', 'sleep_forever', )
 
 import types
-from functools import partial
 
 from kivy.clock import Clock
 Clock_schedule_once = Clock.schedule_once
@@ -9,10 +8,8 @@ Clock_schedule_once = Clock.schedule_once
 
 @types.coroutine
 def sleep(duration):
-    # The partial() here looks meaningless. But this is needed in order
-    # to avoid weak reference
-    args, kwargs = yield lambda step_coro: Clock_schedule_once(
-        partial(step_coro), duration)
+    args, kwargs = yield \
+        lambda step_coro: Clock_schedule_once(step_coro, duration)
     return args[0]
 
 
