9c29d4c38bdcbe0e48d9793eacd588e0b97c7a51
gottadiveintopython <flow4re2c@gmail.com>
2020-05-12 12:39:49 +0900

    re-write test_animation.py

--- tests/test_animation.py
+++ tests/test_animation.py
@@ -8,35 +8,96 @@ def unlimit_maxfps(monkeypatch):
 
 
 @pytest.fixture(scope='module')
-def point_cls():
-    from dataclasses import dataclass
+def target_cls():
+    from dataclasses import dataclass, field
     @dataclass
-    class Point:
-        x: int = 0
-        y: int = 0
-    return Point
+    class Target:
+        num: int = 0
+        lst: list = field(default_factory=lambda:[0, 0])
+        dct: dict = field(default_factory=lambda:{'key': 0})
+    return Target
 
 
 @pytest.fixture()
-def point(point_cls):
-    return point_cls()
+def target(target_cls):
+    return target_cls()
 
 
-def test_skip(point):
-    from math import isclose
-    import time
-    from kivy.clock import Clock
+@pytest.fixture(scope='module')
+def approx():
+    from functools import partial
+    return partial(pytest.approx, abs=1)
+
+
+class Clock:
+    def __init__(self):
+        self.reset()
+    def reset(self):
+        from time import time
+        from kivy.clock import Clock
+        self.last = time()
+        Clock.tick()
+    def sleep(self, duration):
+        from time import time, sleep
+        from kivy.clock import Clock
+        self.last = next = self.last + duration
+        sleep(next - time())
+        Clock.tick()
+
+
+def test_target_itself(target_cls):
+    a = target_cls()
+    b = target_cls()
+    assert a.lst is not b.lst
+    assert a.dct is not b.dct
+
+
+@pytest.mark.parametrize('force_final_value', (True, False, ))
+def test_cancel(target, approx, force_final_value):
     import asynckivy as ak
-
-    Clock.tick()
-    coro = ak.animate(point, x=100, d=1, force_final_value=True)
+    coro = ak.animate(
+        target, num=100, d=.4, force_final_value=force_final_value)
+    clock = Clock()
     ak.start(coro)
 
-    time.sleep(.2)
-    Clock.tick()
-    assert isclose(point.x, 20, abs_tol=1)
-    time.sleep(.2)
-    Clock.tick()
-    assert isclose(point.x, 40, abs_tol=1)
-    coro.close()
-    assert point.x == 100
+    clock.sleep(.1)
+    assert target.num == approx(25)
+    clock.sleep(.1)
+    assert target.num == approx(50)
+    coro.close()  # cancel
+    if force_final_value:
+        assert target.num == approx(100)
+    else:
+        assert target.num == approx(50)
+
+
+def test_animate_list(target, approx):
+    import asynckivy as ak
+    coro = ak.animate(target, lst=[100, 200], d=.4)
+    clock = Clock()
+    ak.start(coro)
+
+    clock.sleep(.1)
+    assert target.lst == approx([25, 50])
+    clock.sleep(.1)
+    assert target.lst == approx([50, 100])
+    clock.sleep(.1)
+    assert target.lst == approx([75, 150])
+    clock.sleep(.1)
+    assert target.lst == approx([100, 200])
+
+
+def test_animate_dict(target, approx):
+    import asynckivy as ak
+    coro = ak.animate(target, dct={'key': 100}, d=.4)
+    clock = Clock()
+    ak.start(coro)
+
+    clock.sleep(.1)
+    assert target.dct == approx({'key': 25})
+    clock.sleep(.1)
+    assert target.dct == approx({'key': 50})
+    clock.sleep(.1)
+    assert target.dct == approx({'key': 75})
+    clock.sleep(.1)
+    assert target.dct == approx({'key': 100})
