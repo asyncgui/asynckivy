88bf3705e32faf8183997efc4b4ce874e7028dc5
gottadiveintopython <flow4re2c@gmail.com>
2020-05-14 23:31:04 +0900

    improve the performance

--- asynckivy/process_and_thread.py
+++ asynckivy/process_and_thread.py
@@ -3,7 +3,7 @@ __all__ = ('thread', 'process', )
 
 async def thread(func, *, daemon=False, polling_interval=3):
     from threading import Thread
-    from ._sleep import sleep
+    from ._sleep import create_sleep
 
     return_value = None
     is_finished = False
@@ -12,15 +12,18 @@ async def thread(func, *, daemon=False, polling_interval=3):
         return_value = func()
         is_finished = True
     Thread(target=wrapper, daemon=daemon).start()
+    sleep = await create_sleep(polling_interval)
     while not is_finished:
-        await sleep(polling_interval)
+        await sleep()
     return return_value
 
 
 async def process(p, *, polling_interval=3):
     '''wait for the completion of subprocess'''
-    from ._sleep import sleep
+    from ._sleep import create_sleep
 
-    while p.poll() is None:
-        await sleep(polling_interval)
+    sleep = await create_sleep(polling_interval)
+    poll = p.poll
+    while poll() is None:
+        await sleep()
     return p.returncode
