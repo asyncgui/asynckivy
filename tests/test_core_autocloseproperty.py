fc05e0899215c3bad9c328b31c705bceb1386e10
gottadiveintopython <flow4re2c@gmail.com>
2020-05-16 13:27:12 +0900

    reduce test code

--- tests/test_core_autocloseproperty.py
+++ tests/test_core_autocloseproperty.py
@@ -5,8 +5,7 @@ import pytest
 def owner_cls():
     from asynckivy import AutoCloseProperty
     class Owner:
-        coro1 = AutoCloseProperty()
-        coro2 = AutoCloseProperty()
+        coro = AutoCloseProperty()
     return Owner
 
 
@@ -21,49 +20,36 @@ async def async_fn():
 
 def test_owner_cls(owner_cls):
     from asynckivy import AutoCloseProperty
-    assert type(owner_cls.coro1) is AutoCloseProperty
-    assert type(owner_cls.coro2) is AutoCloseProperty
-    assert owner_cls.coro1.name == 'coro1'
-    assert owner_cls.coro2.name == 'coro2'
+    assert type(owner_cls.coro) is AutoCloseProperty
+    assert owner_cls.coro.name == 'coro'
 
 
 def test_initial_state(owner):
-    assert owner.coro1 is None
-    assert owner.coro2 is None
-    assert 'coro1' not in owner.__dict__
-    assert 'coro2' not in owner.__dict__
+    assert owner.coro is None
+    assert 'coro' not in owner.__dict__
 
 
 def test_set(owner):
     from inspect import getcoroutinestate, CORO_CLOSED, CORO_CREATED
-    owner.coro1 = coro1a = async_fn()
-    owner.coro2 = coro2 = async_fn()
-    assert owner.coro1 is coro1a
-    assert owner.coro2 is coro2
-    assert owner.__dict__['coro1'] is coro1a
-    assert owner.__dict__['coro2'] is coro2
-    owner.coro1 = coro1b = async_fn()
-    assert getcoroutinestate(coro1a) == CORO_CLOSED
+    owner.coro = coro1 = async_fn()
+    assert getcoroutinestate(coro1) == CORO_CREATED
+    assert owner.coro is coro1
+    assert owner.__dict__['coro'] is coro1
+    owner.coro = coro2 = async_fn()
+    assert getcoroutinestate(coro1) == CORO_CLOSED
     assert getcoroutinestate(coro2) == CORO_CREATED
-    assert owner.coro1 is coro1b
-    assert owner.coro2 is coro2
-    assert owner.__dict__['coro1'] is coro1b
-    assert owner.__dict__['coro2'] is coro2
-    owner.coro1 = None
-    assert getcoroutinestate(coro1b) == CORO_CLOSED
-    assert getcoroutinestate(coro2) == CORO_CREATED
-    assert owner.coro1 is None
-    assert owner.coro2 is coro2
-    assert owner.__dict__['coro1'] is None
-    assert owner.__dict__['coro2'] is coro2
+    assert owner.coro is coro2
+    assert owner.__dict__['coro'] is coro2
+    owner.coro = None
+    assert getcoroutinestate(coro1) == CORO_CLOSED
+    assert getcoroutinestate(coro2) == CORO_CLOSED
+    assert owner.coro is None
+    assert owner.__dict__['coro'] is None
 
 
 def test_delete(owner):
     from inspect import getcoroutinestate, CORO_CLOSED, CORO_CREATED
-    owner.coro1 = coro1 = async_fn()
-    owner.coro2 = coro2 = async_fn()
-    del owner.coro1
-    assert getcoroutinestate(coro1) == CORO_CLOSED
-    assert getcoroutinestate(coro2) == CORO_CREATED
-    assert 'coro1' not in owner.__dict__
-    assert owner.__dict__['coro2'] is coro2
+    owner.coro = coro = async_fn()
+    del owner.coro
+    assert getcoroutinestate(coro) == CORO_CLOSED
+    assert 'coro' not in owner.__dict__
