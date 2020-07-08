98515f471000413fe8630b83c83d043905a9fd8d
gottadiveintopython <flow4re2c@gmail.com>
2020-07-08 22:08:27 +0900

    refactor unit tests * rename some stuff * remove unnecessary import-statement

--- tests/test_rest_of_touch_moves.py
+++ tests/test_rest_of_touch_moves.py
@@ -14,25 +14,24 @@ def touch_cls():
     return Touch
 
 
-@pytest.mark.parametrize('n_touch_move', [0, 1, 10])
-def test_a_number_of_on_touch_move_fired(touch_cls, n_touch_move):
+@pytest.mark.parametrize('n_touch_moves', [0, 1, 10])
+def test_a_number_of_on_touch_moves_fired(touch_cls, n_touch_moves):
     from time import perf_counter
     from kivy.uix.widget import Widget
     import asynckivy as ak
 
     async def _test(w, t):
-        from asynckivy import rest_of_touch_moves 
         n = 0
-        async for __ in rest_of_touch_moves(w, t):
+        async for __ in ak.rest_of_touch_moves(w, t):
             n += 1
-        assert n == n_touch_move
+        assert n == n_touch_moves
         nonlocal done;done = True
         
     done = False
     w = Widget()
     t = touch_cls()
     ak.start(_test(w, t))
-    for __ in range(n_touch_move):
+    for __ in range(n_touch_moves):
         w.dispatch('on_touch_move', t)
     w.dispatch('on_touch_up', t)
     assert done
@@ -43,31 +42,30 @@ def test_break_during_a_for_loop(touch_cls):
     import asynckivy as ak
 
     async def _test(w, t):
-        from asynckivy import rest_of_touch_moves, event
-        nonlocal n
-        async for __ in rest_of_touch_moves(w, t):
-            n += 1
-            if n == 2:
+        nonlocal n_touch_moves
+        async for __ in ak.rest_of_touch_moves(w, t):
+            n_touch_moves += 1
+            if n_touch_moves == 2:
                 break
-        await event(w, 'on_touch_up')
+        await ak.event(w, 'on_touch_up')
         nonlocal done;done = True
 
-    n = 0
+    n_touch_moves = 0
     done = False
     w = Widget()
     t = touch_cls()
     ak.start(_test(w, t))
     w.dispatch('on_touch_move', t)
-    assert n == 1
+    assert n_touch_moves == 1
     assert not done
     w.dispatch('on_touch_move', t)
-    assert n == 2
+    assert n_touch_moves == 2
     assert not done
     w.dispatch('on_touch_move', t)
-    assert n == 2
+    assert n_touch_moves == 2
     assert not done
     w.dispatch('on_touch_up', t)
-    assert n == 2
+    assert n_touch_moves == 2
     assert done
 
 
@@ -83,8 +81,7 @@ def test_eat_touch_events(touch_cls, eats_touch_move, eats_touch_up, expectation
     import asynckivy as ak
 
     async def _test(parent, t):
-        from asynckivy import rest_of_touch_moves 
-        async for __ in rest_of_touch_moves(
+        async for __ in ak.rest_of_touch_moves(
                 parent, t,
                 eats_touch_move=eats_touch_move,
                 eats_touch_up=eats_touch_up):
