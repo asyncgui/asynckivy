5049d2909fcbab50a02f55b5c5c1d52b90603187
gottadiveintopython <flow4re2c@gmail.com>
2020-10-06 11:15:34 +0900

    remove duplicated code

--- asynckivy/adaptor/to_trio.py
+++ asynckivy/adaptor/to_trio.py
@@ -10,23 +10,9 @@ from asynckivy.exceptions import CancelledError
 
 
 async def _ak_awaitable_wrapper(
-        outcome:dict, end_signal:asynckivy.Event, ak_awaitable):
+        outcome:dict, end_signal:trio.Event, ak_awaitable):
     try:
         outcome['return_value'] = await ak_awaitable
-    except GeneratorExit:
-        outcome['cancelled'] = True
-        raise
-    except Exception as e:
-        outcome['exception'] = e
-    finally:
-        end_signal.set()
-
-
-async def _ak_asyncfn_wrapper(
-        outcome:dict, end_signal:asynckivy.Event, ak_asyncfn,
-        *args, **kwargs):
-    try:
-        outcome['return_value'] = await ak_asyncfn(*args, **kwargs)
     except GeneratorExit:
         outcome['cancelled'] = True
         raise
@@ -41,8 +27,11 @@ async def run_awaitable(
     '''(experimental)
     Run an asynckivy-flavored awaitable under Trio.
     
-    Usage:
-        nursery.start_soon(run_awaitable, asynckivy_flavored_awaitable)
+    Usage #1:
+        nursery.start_soon(run_awaitable, an_asynckivy_awaitable)
+
+    Usage #2:
+        return_value = await run_awaitable(an_asynckivy_awaitable)
     '''
     end_signal = trio.Event()
     try:
@@ -62,34 +51,18 @@ async def run_awaitable(
         wrapper_coro.close()
 
 
-def callable_to_asyncfn(ak_asyncfn):
+def callable_to_asyncfn(ak_callable):
     '''(experimental)
     Convert a callable that returns a asynckivy-flavored awaitable to
-    Trio-flavored async function.
+    a Trio-flavored async function.
 
     Usage:
-        trio_asyncfn = callable_to_asyncfn(asynckivy_asyncfn)
-        nursery.start_soon(trio_asyncfn)
+        a_trio_asyncfn = callable_to_asyncfn(an_asynckivy_asyncfn)
     '''
-    if not callable(ak_asyncfn):
-        raise ValueError(f"{ak_asyncfn} is not callable")
-    end_signal = trio.Event()
+    if not callable(ak_callable):
+        raise ValueError(f"{ak_callable} is not callable")
     async def trio_asyncfn(*args, **kwargs):
         task_status = kwargs.pop('task_status', trio.TASK_STATUS_IGNORED)
-        try:
-            outcome = {}
-            wrapper_coro = _ak_asyncfn_wrapper(
-                outcome, end_signal, ak_asyncfn, *args, **kwargs)
-            asynckivy.start(wrapper_coro)
-            task_status.started(wrapper_coro)
-            await end_signal.wait()
-            exception = outcome.get('exception', None)
-            if exception is not None:
-                raise exception
-            if outcome.get('cancelled', False):
-                raise CancelledError("Inner task was cancelled")
-            return outcome['return_value']
-
-        finally:
-            wrapper_coro.close()
+        return await run_awaitable(
+            ak_callable(*args, **kwargs), task_status=task_status, )
     return trio_asyncfn
