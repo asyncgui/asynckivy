de37a5b591a9528149e69a9729ac80e56714526b
gottadiveintopython <flow4re2c@gmail.com>
2020-10-27 18:09:43 +0900

    add 'fade_transition()'

---
+++ examples/changing_text_with_fade_transition.py
@@ -0,0 +1,31 @@
+from kivy.app import App
+from kivy.uix.label import Label
+import asynckivy as ak
+
+
+class TestApp(App):
+    def build(self):
+        return Label(font_size=40)
+
+    def on_start(self):
+        async def main_task(label):
+            from kivy.utils import get_random_color
+            await ak.sleep(1)
+            for text in (
+                'Zen of Python',
+                'Beautiful is better than ugly.',
+                'Explicit is better than implicit.',
+                'Simple is better than complex.',
+                'Complex is better than complicated.',
+                '',
+            ):
+                async with ak.fade_transition(label):
+                    label.text = text
+                    label.color = get_random_color()
+                await ak.event(label, 'on_touch_down')
+            self.stop()
+        ak.start(main_task(self.root))
+
+
+if __name__ == '__main__':
+    TestApp().run()
