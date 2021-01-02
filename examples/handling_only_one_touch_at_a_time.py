b7012df640962c9da057a448684f319093083ac4
gottadiveintopython <flow4re2c@gmail.com>
2021-01-02 20:18:29 +0900

    refactor 'handling_only_one_touch_at_a_time.py'

--- examples/handling_only_one_touch_at_a_time.py
+++ examples/handling_only_one_touch_at_a_time.py
@@ -1,27 +1,58 @@
+'''
+Painter
+=======
+
+* can draw only rectangles
+* can handle only one touch at a time
+'''
+
+from kivy.uix.relativelayout import RelativeLayout
 from kivy.app import runTouchApp
-import asynckivy as ak
-
-try:
-    from .handling_multiple_touches_at_a_time import Painter as OriginalPainter
-except ImportError:
-    from handling_multiple_touches_at_a_time import Painter as OriginalPainter
 
 
-class Painter(OriginalPainter):
-    def on_touch_down(self, touch):
-        pass
-
+class Painter(RelativeLayout):
     def on_kv_post(self, *args, **kwargs):
-        ak.start(self.keep_watching_touch_events())
+        import asynckivy
+        asynckivy.start(self._async_main())
 
-    async def keep_watching_touch_events(self):
+    async def _async_main(self):
+        from asynckivy import rest_of_touch_moves, event
+        from kivy.graphics import Line, Color, Rectangle, InstructionGroup
+        from kivy.utils import get_random_color
+
+        def will_accept_touch(w, t) -> bool:
+            return w.collide_point(*t.opos) and (not t.is_mouse_scrolling)
+
         while True:
-            __, touch = await ak.event(
-                self, 'on_touch_down',
-                filter=lambda w, t: w.collide_point(*t.opos),
-                return_value=True,
-            )
-            await self.draw_rect(touch)
+            __, touch = await event(
+                self, 'on_touch_down', filter=will_accept_touch,
+                return_value=True)
+            inst_group = InstructionGroup()
+            self.canvas.add(inst_group)
+            inst_group.add(Color(*get_random_color()))
+            line = Line(width=2)
+            inst_group.add(line)
+            ox, oy = self.to_local(*touch.opos)
+            on_touch_move_was_fired = False
+            async for __ in rest_of_touch_moves(self, touch):
+                # Don't await anything during the iteration
+                on_touch_move_was_fired = True
+                x, y = self.to_local(*touch.pos)
+                min_x = min(x, ox)
+                min_y = min(y, oy)
+                max_x = max(x, ox)
+                max_y = max(y, oy)
+                line.rectangle = [min_x, min_y, max_x - min_x, max_y - min_y]
+            if on_touch_move_was_fired:
+                inst_group.add(Color(*get_random_color(alpha=.3)))
+                inst_group.add(
+                    Rectangle(
+                        pos=(min_x, min_y),
+                        size=(max_x - min_x, max_y - min_y, ),
+                    )
+                )
+            else:
+                self.canvas.remove(inst_group)
 
 
 if __name__ == "__main__":
