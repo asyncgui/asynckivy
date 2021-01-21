64696926724536ae7d2e4cd3873f51d96fd77c1a
gottadiveintopython <flow4re2c@gmail.com>
2021-01-21 13:58:06 +0900

    add 'close_soon()'

---
+++ asynckivy/_close_soon.py
@@ -0,0 +1,26 @@
+__all__ = ('close_soon', )
+
+from kivy.clock import Clock
+
+
+_waiting = []
+
+
+def _close(dt):
+    global _waiting
+    waiting = _waiting
+    _waiting = []
+    for coro_or_task in waiting:
+        coro_or_task.close()
+
+
+_trigger_close = Clock.create_trigger(_close, 0)
+
+
+def close_soon(coro_or_task):
+    '''
+    Schedules a coroutine/Task to close after the next frame.
+    '''
+    _waiting.append(coro_or_task)
+    _trigger_close()
+    return coro_or_task
