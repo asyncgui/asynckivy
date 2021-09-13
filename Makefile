6185f911b3ea4ba972b91958ce887884871861ad
gottadiveintopython <flow4re2c@gmail.com>
2021-09-13 14:58:33 +0900

    refactor some tests

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@ PYTEST = $(PYTHON) -m pytest
 FLAKE8 = $(PYTHON) -m flake8
 
 test:
-	$(PYTEST) ./tests
+	env KCFG_GRAPHICS_MAXFPS=0 $(PYTEST) ./tests
 
 test_free_only_clock:
-	env KCFG_KIVY_KIVY_CLOCK=free_only $(PYTEST) ./tests
+	env KCFG_GRAPHICS_MAXFPS=0 KCFG_KIVY_KIVY_CLOCK=free_only $(PYTEST) ./tests
 
 style:
 	$(FLAKE8) --count --select=E9,F63,F7,F82 --show-source --statistics ./tests ./asynckivy ./examples
--- tests/test_animation.py
+++ tests/test_animation.py
@@ -1,13 +1,6 @@
 import pytest
 
 
-@pytest.fixture(autouse=True)
-def unlimit_maxfps(monkeypatch):
-    '''not sure this fixture actually works'''
-    from kivy.clock import Clock
-    monkeypatch.setattr(Clock, '_max_fps', 0)
-
-
 @pytest.fixture(scope='module')
 def approx():
     from functools import partial
@@ -21,70 +14,54 @@ class Target:
         self.dct = {'key': 0, }
 
 
-class Clock:
-    def __init__(self):
-        self.reset()
-    def reset(self):
-        from time import time
-        from kivy.clock import Clock
-        self.last = time()
-        Clock.tick()
-    def sleep(self, duration):
-        from time import time, sleep
-        from kivy.clock import Clock
-        self.last = next = self.last + duration
-        sleep(next - time())
-        Clock.tick()
-
-
-def test_cancel(approx):
+def test_cancel(approx, ready_to_sleep):
     import asynckivy as ak
     target = Target()
     coro = ak.animate(target, num=100, d=.4,)
-    clock = Clock()
+    sleep = ready_to_sleep()
     ak.raw_start(coro)
 
-    clock.sleep(.1)
+    sleep(.1)
     assert target.num == approx(25)
-    clock.sleep(.1)
+    sleep(.1)
     assert target.num == approx(50)
     coro.close()  # cancel
     assert target.num == approx(50)
 
 
-def test_list(approx):
+def test_list(approx, ready_to_sleep):
     import asynckivy as ak
     target = Target()
     coro = ak.animate(target, lst=[100, 200], d=.4)
-    clock = Clock()
+    sleep = ready_to_sleep()
     ak.start(coro)
 
-    clock.sleep(.1)
+    sleep(.1)
     assert target.lst == approx([25, 50])
-    clock.sleep(.1)
+    sleep(.1)
     assert target.lst == approx([50, 100])
-    clock.sleep(.1)
+    sleep(.1)
     assert target.lst == approx([75, 150])
-    clock.sleep(.1)
+    sleep(.1)
     assert target.lst == approx([100, 200])
-    clock.sleep(.1)
+    sleep(.1)
     assert target.lst == approx([100, 200])
 
 
-def test_dict(approx):
+def test_dict(approx, ready_to_sleep):
     import asynckivy as ak
     target = Target()
     coro = ak.animate(target, dct={'key': 100}, d=.4)
-    clock = Clock()
+    sleep = ready_to_sleep()
     ak.start(coro)
 
-    clock.sleep(.1)
+    sleep(.1)
     assert target.dct == approx({'key': 25})
-    clock.sleep(.1)
+    sleep(.1)
     assert target.dct == approx({'key': 50})
-    clock.sleep(.1)
+    sleep(.1)
     assert target.dct == approx({'key': 75})
-    clock.sleep(.1)
+    sleep(.1)
     assert target.dct == approx({'key': 100})
-    clock.sleep(.1)
+    sleep(.1)
     assert target.dct == approx({'key': 100})
