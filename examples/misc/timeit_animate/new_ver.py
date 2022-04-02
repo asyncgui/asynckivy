7e10cd8c1e412d8502081b984580426bafe361d0
Nattōsai Mitō <flow4re2c@gmail.com>
2022-04-02 14:25:52 +0900

    reduce redandant

--- examples/misc/timeit_animate/new_ver.py
+++ examples/misc/timeit_animate/new_ver.py
@@ -1,6 +1,7 @@
 __all__ = ('create_update', )
 from functools import partial
 from kivy.animation import AnimationTransition
+from asynckivy._animation import _update
 
 
 def create_update(target, **kwargs):
@@ -25,47 +26,3 @@ def create_update(target, **kwargs):
         properties[key] = (original_value, value)
 
     return partial(_update, target, duration, transition, properties, lambda: None, [0., ])
-
-
-def _calculate(a, b, t, isinstance=isinstance, list=list, tuple=tuple, dict=dict):
-    '''The logic of this function is identical to 'kivy.animation.Animation._calculate()'
-    '''
-    if isinstance(a, list) or isinstance(a, tuple):
-        if isinstance(a, list):
-            tp = list
-        else:
-            tp = tuple
-        return tp([_calculate(a[x], b[x], t) for x in range(len(a))])
-    elif isinstance(a, dict):
-        d = {}
-        for x in a:
-            if x not in b:
-                # User requested to animate only part of the dict.
-                # Copy the rest
-                d[x] = a[x]
-            else:
-                d[x] = _calculate(a[x], b[x], t)
-        return d
-    else:
-        return (a * (1. - t)) + (b * t)
-
-
-def _update(target, duration, transition, properties, step_coro, p_time, dt, _calculate=_calculate, setattr=setattr):
-    time = p_time[0] + dt
-    p_time[0] = time
-
-    # calculate progression
-    progress = min(1., time / duration)
-    t = transition(progress)
-
-    # apply progression on target
-    target = target
-    for key, values in properties.items():
-        a, b = values
-        value = _calculate(a, b, t)
-        setattr(target, key, value)
-
-    # time to stop ?
-    if progress >= 1.:
-        step_coro()
-        return False
