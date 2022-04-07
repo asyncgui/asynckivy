fce08e5e0d6a878aa9d592ef4c0ac666d93fd5cd
Nattōsai Mitō <flow4re2c@gmail.com>
2022-04-07 17:14:21 +0900

    refactor 'rest_of_touch_moves()'

--- asynckivy/_rest_of_touch_moves.py
+++ asynckivy/_rest_of_touch_moves.py
@@ -1,10 +1,43 @@
 __all__ = ('rest_of_touch_moves', )
 
 import types
+from functools import partial
+from asynckivy import or_, sleep, event, get_step_coro, MotionEventAlreadyEndedError
 
 
-async def rest_of_touch_moves(
-        widget, touch, *, stop_dispatching=False, timeout=1.):
+def _on_touch_up_sd(step_coro, touch, w, t):
+    if t is touch:
+        if t.grab_current is w:
+            t.ungrab(w)
+            step_coro(False)
+        return True
+
+
+def _on_touch_move_sd(step_coro, touch, w, t):
+    if t is touch:
+        if t.grab_current is w:
+            step_coro(True)
+        return True
+
+
+def _on_touch_up(step_coro, touch, w, t):
+    if t.grab_current is w and t is touch:
+        t.ungrab(w)
+        step_coro(False)
+        return True
+
+
+def _on_touch_move(step_coro, touch, w, t):
+    if t.grab_current is w and t is touch:
+        step_coro(True)
+        return True
+
+
+_callbacks = ((_on_touch_up_sd, _on_touch_move_sd, ), (_on_touch_up, _on_touch_move, ), )
+del _on_touch_up, _on_touch_move, _on_touch_up_sd, _on_touch_move_sd
+
+
+async def rest_of_touch_moves(widget, touch, *, stop_dispatching=False, timeout=1.):
     '''
     Returns an async-generator that iterates the number of times
     `on_touch_move` is fired, and ends when `on_touch_up` is fired. Grabs and
@@ -13,10 +46,6 @@ async def rest_of_touch_moves(
     touch until the generator ends. If `on_touch_up` was already fired,
     `MotionEventAlreadyEndedError` will be raised.
     '''
-    from asynckivy import (
-        or_, sleep, event, get_step_coro, MotionEventAlreadyEndedError,
-    )
-
     if touch.time_end != -1:
         # `on_touch_up` might have been already fired so we need to find out
         # it actually was or not.
@@ -31,40 +60,18 @@ async def rest_of_touch_moves(
             return
 
     step_coro = await get_step_coro()
-    if stop_dispatching:
-        def _on_touch_up(w, t):
-            if t is touch:
-                if t.grab_current is w:
-                    t.ungrab(w)
-                    step_coro(False)
-                return True
-
-        def _on_touch_move(w, t):
-            if t is touch:
-                if t.grab_current is w:
-                    step_coro(True)
-                return True
-    else:
-        def _on_touch_up(w, t):
-            if t.grab_current is w and t is touch:
-                t.ungrab(w)
-                step_coro(False)
-                return True
-
-        def _on_touch_move(w, t):
-            if t.grab_current is w and t is touch:
-                step_coro(True)
-                return True
+    on_touch_up, on_touch_move = _callbacks[not stop_dispatching]
+    on_touch_up = partial(on_touch_up, step_coro, touch)
+    on_touch_move = partial(on_touch_move, step_coro, touch)
 
     touch.grab(widget)
-    uid_up = widget.fbind('on_touch_up', _on_touch_up)
-    uid_move = widget.fbind('on_touch_move', _on_touch_move)
+    uid_up = widget.fbind('on_touch_up', on_touch_up)
+    uid_move = widget.fbind('on_touch_move', on_touch_move)
     assert uid_up
     assert uid_move
 
-    # assigning to a local variable might improve performance
-    true_if_touch_move_false_if_touch_up = \
-        _true_if_touch_move_false_if_touch_up
+    # LOAD_FAST
+    true_if_touch_move_false_if_touch_up = _true_if_touch_move_false_if_touch_up
 
     try:
         while await true_if_touch_move_false_if_touch_up():
