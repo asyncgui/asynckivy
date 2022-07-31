7ecd0272fae56a03363d883498b818395d62a9d3
Nattōsai Mitō <flow4re2c@gmail.com>
2022-08-01 06:26:37 +0900

    remove 'create_sleep()'

--- tests/test_sleep.py
+++ tests/test_sleep.py
@@ -68,31 +68,3 @@ def test_repeat_sleeping(free_await):
     Clock.tick()
     assert task_state == 'C'
     assert task.done
-
-
-def test_create_sleep():
-    import time
-    from kivy.clock import Clock
-    import asynckivy as ak
-
-    async def _task():
-        nonlocal task_state
-        sleep = await ak.create_sleep(.5)
-        task_state = 'A'
-        await sleep()
-        task_state = 'B'
-        await sleep()
-        task_state = 'C'
-
-    task_state = None
-    Clock.tick()
-    ak.start(_task())
-    time.sleep(.2)
-    Clock.tick()
-    assert task_state == 'A'
-    time.sleep(.5)
-    Clock.tick()
-    assert task_state == 'B'
-    time.sleep(.5)
-    Clock.tick()
-    assert task_state == 'C'
