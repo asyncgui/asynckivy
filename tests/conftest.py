c5be8c1a4ee6dc93ad337de6501bc4124e436cff
Nattōsai Mitō <flow4re2c@gmail.com>
2022-08-03 12:35:31 +0900

    refactor animation-related unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,23 +2,23 @@ import pytest
 from kivy.tests.fixtures import kivy_clock
 
 
-@pytest.fixture()
-def ready_to_sleep():
+@pytest.fixture(scope='session')
+def _sleep_then_tick():
+    from functools import partial
     from kivy.clock import Clock
-    from time import time, sleep
+    import time 
 
-    last = None
+    def f(time_sleep, clock_time, clock_get_time, clock_tick, duration):
+        # clock_get_time() returns the last time the clock ticked.
+        # clock_time() returns the current time.
+        time_sleep(clock_get_time() + duration - clock_time())
+        clock_tick()
 
-    def sleep_then_tick(duration):
-        nonlocal last
-        last += duration
-        sleep(last - time())
-        Clock.tick()
+    return partial(f, time.sleep, Clock.time, Clock.get_time, Clock.tick)
 
-    def ready_to_sleep():
-        nonlocal last
-        Clock.tick()
-        last = time()
-        return sleep_then_tick
 
-    return ready_to_sleep
+@pytest.fixture()
+def sleep_then_tick(_sleep_then_tick):
+    from kivy.clock import Clock
+    Clock.tick()
+    return _sleep_then_tick
