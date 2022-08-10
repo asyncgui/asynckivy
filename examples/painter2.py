b1382bff789a735ef7762f11c47e0825e656fcf7
Nattōsai Mitō <flow4re2c@gmail.com>
2022-08-10 11:47:01 +0900

    Simplify the painter examples. They no longer fill rectangles.

--- examples/painter2.py
+++ examples/painter2.py
@@ -22,18 +22,16 @@ class Painter(RelativeLayout):
 
     async def main(self):
         from asynckivy import rest_of_touch_moves, event
-        from kivy.graphics import Line, Color, Rectangle, InstructionGroup
+        from kivy.graphics import Line, Color
         from kivy.utils import get_random_color
 
         will_accept_touch = self.will_accept_touch
         self_to_local = self.to_local
         while True:
             __, touch = await event(self, 'on_touch_down', filter=will_accept_touch, stop_dispatching=True)
-            inst_group = InstructionGroup()
-            self.canvas.add(inst_group)
-            inst_group.add(Color(*get_random_color()))
-            line = Line(width=2)
-            inst_group.add(line)
+            with self.canvas:
+                Color(*get_random_color())
+                line = Line(width=2)
             ox, oy = x, y = self_to_local(*touch.opos)
             async for __ in rest_of_touch_moves(self, touch, stop_dispatching=True):
                 # Don't await anything during the iteration
@@ -41,16 +39,6 @@ class Painter(RelativeLayout):
                 min_x, max_x = (x, ox) if x < ox else (ox, x)
                 min_y, max_y = (y, oy) if y < oy else (oy, y)
                 line.rectangle = (min_x, min_y, max_x - min_x, max_y - min_y, )
-            if x == ox and y == oy:
-                self.canvas.remove(inst_group)
-            else:
-                inst_group.add(Color(*get_random_color(alpha=.3)))
-                inst_group.add(
-                    Rectangle(
-                        pos=(min_x, min_y),
-                        size=(max_x - min_x, max_y - min_y, ),
-                    )
-                )
 
 
 if __name__ == "__main__":
