416c18b9b31bdc1c5b2a189c1a728581b0824925
Nattōsai Mitō <flow4re2c@gmail.com>
2022-08-12 18:50:05 +0900

    use the 'sleep_then_tick' fixture on the sleep-related unittests

--- tests/test_sleep.py
+++ tests/test_sleep.py
@@ -1,47 +1,32 @@
 import pytest
 
-def test_sleep():
-    import time
-    from kivy.clock import Clock
+def test_sleep(sleep_then_tick):
     import asynckivy as ak
 
-    Clock.tick()
     task = ak.start(ak.sleep(.1))
     assert not task.done
-    Clock.tick()
+    sleep_then_tick(.05)
     assert not task.done
-    time.sleep(.07)
-    Clock.tick()
-    assert not task.done
-    time.sleep(.07)
-    Clock.tick()
+    sleep_then_tick(.06)
     assert task.done
 
 
-def test_sleep_free():
-    import time
+def test_sleep_free(sleep_then_tick):
     from kivy.clock import Clock
     import asynckivy as ak
 
     if not hasattr(Clock, 'create_trigger_free'):
         pytest.skip("free-type Clock is not available")
-    Clock.tick()
     task = ak.start(ak.sleep_free(.1))
     assert not task.done
-    Clock.tick()
+    sleep_then_tick(.05)
     assert not task.done
-    time.sleep(.07)
-    Clock.tick()
-    assert not task.done
-    time.sleep(.07)
-    Clock.tick()
+    sleep_then_tick(.06)
     assert task.done
 
 
 @pytest.mark.parametrize('free_await', (False, True))
-def test_repeat_sleeping(free_await):
-    import time
-    from kivy.clock import Clock
+def test_repeat_sleeping(sleep_then_tick, free_await):
     import asynckivy as ak
 
     async def async_fn():
@@ -54,17 +39,13 @@ def test_repeat_sleeping(free_await):
             task_state = 'C'
 
     task_state = None
-    Clock.tick()
     task = ak.start(async_fn())
-    time.sleep(.2)
-    Clock.tick()
+    sleep_then_tick(.2)
     assert task_state == 'A'
     assert not task.done
-    time.sleep(.5)
-    Clock.tick()
+    sleep_then_tick(.5)
     assert task_state == 'B'
     assert not task.done
-    time.sleep(.5)
-    Clock.tick()
+    sleep_then_tick(.5)
     assert task_state == 'C'
     assert task.done
