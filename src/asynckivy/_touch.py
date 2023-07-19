46f54c611171e3e06279ebe3448d6055f7f36a40
Nattōsai Mitō <flow4re2c@gmail.com>
2023-07-19 18:42:21 +0900

    add 'touch_up_event()'

--- src/asynckivy/_touch.py
+++ src/asynckivy/_touch.py
@@ -1,8 +1,12 @@
-__all__ = ('watch_touch', 'rest_of_touch_moves', 'rest_of_touch_events', )
+__all__ = ('watch_touch', 'rest_of_touch_moves', 'rest_of_touch_events', 'touch_up_event', )
 
 import typing as T
 import types
 import functools
+from asyncgui import wait_any, sleep_forever
+from ._exceptions import MotionEventAlreadyEndedError
+from ._sleep import sleep
+from ._event import event
 import asynckivy as ak
 
 
@@ -95,7 +99,7 @@ class watch_touch:
         widget = self._widget
         if touch.time_end != -1:
             # `on_touch_up` might have been already fired so we need to find out it actually was or not.
-            tasks = await ak.wait_any(
+            tasks = await wait_any(
                 ak.sleep(self._timeout),
                 ak.event(widget, 'on_touch_up', filter=lambda w, t: t is touch),
             )
@@ -121,6 +125,57 @@ class watch_touch:
         self._touch.ungrab(w)
         w.unbind_uid('on_touch_up', self._uid_up)
         w.unbind_uid('on_touch_move', self._uid_move)
+
+
+async def touch_up_event(widget, touch, *, stop_dispatching=False, timeout=1.) -> T.Awaitable:
+    '''
+    *(experimental state)*
+
+    Return an awaitable that can be used to wait for the ``on_touch_up`` event of the given ``touch`` to occur. 
+
+    .. code-block::
+
+        __, touch = await event(widget, 'on_touch_down')
+        ...
+        await touch_up_event(widget, touch)
+
+    You might wonder what's the differences compared to the following code:
+
+    .. code-block::
+        :emphasize-lines: 3
+
+        __, touch = await event(widget, 'on_touch_down')
+        ...
+        await event(widget, 'on_touch_up', filter=lambda w, t: t is touch)
+
+    The latter has two problems:
+    If the ``on_touch_up`` event of the ``touch`` occurred prior to the emphasized line,
+    the execution will stop at that line.
+    Even if the event didn't occur prior to that line, the execution still might stop because
+    `Kivy does not guarantee`_ that all touch events are delivered to all widgets.
+
+    This api takes care of both problems in the same way as :func:`watch_touch`.
+    If the ``on_touch_up`` event has already occurred, it raises an :exc:`MotionEventAlreadyEndedError` exception.
+    And it grabs/ungrabs the ``touch``.
+
+    Needless to say, if you want to wait for both ``on_touch_move`` and ``on_touch_up`` events at the same time,
+    use :func:`watch_touch` or :func:`rest_of_touch_events` instead.
+
+    .. _Kivy does not guarantee: https://kivy.org/doc/stable/guide/inputs.html#grabbing-touch-events
+    '''
+    touch.grab(widget)
+    try:
+        tasks = await wait_any(
+            sleep(timeout) if touch.time_end != -1 else sleep_forever(),
+            event(
+                widget, 'on_touch_up', stop_dispatching=stop_dispatching,
+                filter=lambda w, t: t.grab_current is w and t is touch,
+            ),
+        )
+        if tasks[0].finished:
+            raise MotionEventAlreadyEndedError(f"MotionEvent(uid={touch.uid}) has already ended")
+    finally:
+        touch.ungrab(widget)
 
 
 async def rest_of_touch_moves(widget, touch, *, stop_dispatching=False, timeout=1.) -> T.AsyncIterator[None]:
