e288fa7e9072304e0b37ad448325740c7b8039ff
Nattōsai Mitō <flow4re2c@gmail.com>
2023-10-09 12:19:52 +0900

    refactor the painter examples

--- examples/painter.py
+++ examples/painter.py
@@ -9,7 +9,7 @@ from functools import cached_property
 from kivy.graphics import Line, Color
 from kivy.utils import get_random_color
 from kivy.uix.relativelayout import RelativeLayout
-from kivy.app import runTouchApp
+from kivy.app import App
 import asynckivy as ak
 
 
@@ -18,11 +18,11 @@ class Painter(RelativeLayout):
     def _ud_key(self):
         return 'Painter.' + str(self.uid)
 
-    def will_accept_touch(self, touch) -> bool:
+    def accepts_touch(self, touch) -> bool:
         return self.collide_point(*touch.opos) and (not touch.is_mouse_scrolling) and (self._ud_key not in touch.ud)
 
     def on_touch_down(self, touch):
-        if self.will_accept_touch(touch):
+        if self.accepts_touch(touch):
             touch.ud[self._ud_key] = True
             ak.start(self.draw_rect(touch))
             return True
@@ -34,12 +34,17 @@ class Painter(RelativeLayout):
         self_to_local = self.to_local
         ox, oy = self_to_local(*touch.opos)
         async for __ in ak.rest_of_touch_events(self, touch, stop_dispatching=True):
-            # Don't await anything during the iteration
+            # Don't await anything during the loop
             x, y = self_to_local(*touch.pos)
             min_x, max_x = (x, ox) if x < ox else (ox, x)
             min_y, max_y = (y, oy) if y < oy else (oy, y)
             line.rectangle = (min_x, min_y, max_x - min_x, max_y - min_y, )
 
 
+class SampleApp(App):
+    def build(self):
+        return Painter()
+
+
 if __name__ == "__main__":
-    runTouchApp(Painter())
+    SampleApp(title='Painter').run()
--- examples/painter2.py
+++ examples/painter2.py
@@ -5,40 +5,46 @@ Painter
 * can only handle one touch at a time
 '''
 
+from functools import partial
 from kivy.uix.relativelayout import RelativeLayout
-from kivy.app import runTouchApp
+from kivy.app import App
+import asynckivy as ak
 
 
 class Painter(RelativeLayout):
-    def on_kv_post(self, *args, **kwargs):
-        import asynckivy
-        super().on_kv_post(*args, **kwargs)
-        asynckivy.start(self.main())
-
     @staticmethod
-    def will_accept_touch(widget, touch) -> bool:
+    def accepts_touch(widget, touch) -> bool:
         return widget.collide_point(*touch.opos) and (not touch.is_mouse_scrolling)
 
     async def main(self):
-        from asynckivy import rest_of_touch_events, event
+        from asynckivy import rest_of_touch_events
         from kivy.graphics import Line, Color
         from kivy.utils import get_random_color
 
-        will_accept_touch = self.will_accept_touch
         self_to_local = self.to_local
+        touch_down_event = partial(ak.event, self, 'on_touch_down', filter=self.accepts_touch, stop_dispatching=True)
+
         while True:
-            __, touch = await event(self, 'on_touch_down', filter=will_accept_touch, stop_dispatching=True)
+            __, touch = await touch_down_event()
             with self.canvas:
                 Color(*get_random_color())
                 line = Line(width=2)
             ox, oy = self_to_local(*touch.opos)
             async for __ in rest_of_touch_events(self, touch, stop_dispatching=True):
-                # Don't await anything during the iteration
+                # Don't await anything during the loop
                 x, y = self_to_local(*touch.pos)
                 min_x, max_x = (x, ox) if x < ox else (ox, x)
                 min_y, max_y = (y, oy) if y < oy else (oy, y)
                 line.rectangle = (min_x, min_y, max_x - min_x, max_y - min_y, )
 
 
+class SampleApp(App):
+    def build(self):
+        return Painter()
+
+    def on_start(self):
+        ak.start(self.root.main())
+
+
 if __name__ == "__main__":
-    runTouchApp(Painter())
+    SampleApp(title='Painter').run()
