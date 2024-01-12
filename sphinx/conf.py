82893895fcbf3172a83e01c7ce2d0648db4369b8
Nattōsai Mitō <flow4re2c@gmail.com>
2024-01-12 13:13:24 +0900

    swich to 'furo' theme

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -33,12 +33,10 @@ gettext_location = False
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-html_theme = "sphinx_book_theme"
+html_theme = "furo"
 html_static_path = ['_static']
 html_theme_options = {
-    "use_repository_button": True,
-    "repository_url": r"https://github.com/asyncgui/asynckivy",
-    "use_download_button": False,
+    "top_of_page_button": "edit",
 }
 
 
