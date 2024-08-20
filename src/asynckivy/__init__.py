61028460b9c51af18f96c2919eb9f3247597eb2c
Nattōsai Mitō <flow4re2c@gmail.com>
2024-08-20 20:26:57 +0900

    (api-break) remove 'animate'

--- src/asynckivy/__init__.py
+++ src/asynckivy/__init__.py
@@ -7,7 +7,6 @@ __all__ = (
     'anim_with_dt_et_ratio',
     'anim_with_et',
     'anim_with_ratio',
-    'animate',
     'create_texture_from_text',
     'event',
     'fade_transition',
@@ -33,7 +32,6 @@ from ._exceptions import MotionEventAlreadyEndedError
 from ._sleep import sleep, sleep_free, repeat_sleeping, move_on_after
 from ._event import event
 from ._anim_with_xxx import anim_with_dt, anim_with_et, anim_with_ratio, anim_with_dt_et, anim_with_dt_et_ratio
-from ._animation import animate
 from ._anim_attrs import anim_attrs, anim_attrs_abbr
 from ._interpolate import interpolate, fade_transition
 from ._touch import watch_touch, rest_of_touch_events, touch_up_event
