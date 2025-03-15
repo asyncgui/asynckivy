9029a6b387b33a93471b5e1d96581b99f14c1c6b
Nattōsai Mitō <flow4re2c@gmail.com>
2025-03-16 04:35:51 +0900

    Ensure unit tests are not affected by real-time.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,11 @@ from kivy.tests.fixtures import kivy_clock
 
 @pytest.fixture()
 def sleep_then_tick(kivy_clock):
-    from functools import partial
-    import time 
+    current_time = kivy_clock.time()
+    kivy_clock.time = lambda: current_time
 
-    def f(time_sleep, clock_time, clock_get_time, clock_tick, duration):
-        # clock_get_time() returns the last time the clock ticked.
-        # clock_time() returns the current time.
-        time_sleep(clock_get_time() + duration - clock_time())
-        clock_tick()
-
-    return partial(f, time.sleep, kivy_clock.time, kivy_clock.get_time, kivy_clock.tick)
+    def sleep_then_tick(duration):
+        nonlocal current_time
+        current_time += duration
+        kivy_clock.tick()
+    return sleep_then_tick
