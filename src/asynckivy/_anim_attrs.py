6edda10587d495b343f7829a0a6821abab9cc9ca
Nattōsai Mitō <flow4re2c@gmail.com>
2025-07-05 16:51:11 +0900

    (api-break) Remove the options to specify the output sequence type, and replace `sequence_type(<generator expression>)` with a list comprehension

--- src/asynckivy/_anim_attrs.py
+++ src/asynckivy/_anim_attrs.py
@@ -6,7 +6,7 @@ from kivy.animation import AnimationTransition
 import asyncgui
 
 
-def _update(setattr, zip, min, obj, duration, transition, output_seq_type, anim_params, task, p_time, dt):
+def _update(setattr, zip, min, obj, duration, transition, anim_params, task, p_time, dt):
     time = p_time[0] + dt
     p_time[0] = time
 
@@ -17,10 +17,10 @@ def _update(setattr, zip, min, obj, duration, transition, output_seq_type, anim_
     # apply progression on obj
     for attr_name, org_value, slope, is_seq in anim_params:
         if is_seq:
-            new_value = output_seq_type(
+            new_value = [
                 slope_elem * t + org_elem
                 for org_elem, slope_elem in zip(org_value, slope)
-            )
+            ]
             setattr(obj, attr_name, new_value)
         else:
             setattr(obj, attr_name, slope * t + org_value)
@@ -36,7 +36,7 @@ _update = partial(_update, setattr, zip, min)
 
 @types.coroutine
 def _anim_attrs(
-        obj, duration, step, transition, output_seq_type, animated_properties,
+        obj, duration, step, transition, animated_properties,
         getattr=getattr, isinstance=isinstance, tuple=tuple, str=str, partial=partial, native_seq_types=(tuple, list),
         zip=zip, Clock=kivy.clock.Clock, AnimationTransition=AnimationTransition,
         _update=_update, _current_task=asyncgui._current_task, _sleep_forever=asyncgui._sleep_forever, /):
@@ -44,22 +44,21 @@ def _anim_attrs(
         transition = getattr(AnimationTransition, transition)
 
     # get current values & calculate slopes
-    anim_params = tuple(
+    anim_params = [
         (
             org_value := getattr(obj, attr_name),
             is_seq := isinstance(org_value, native_seq_types),
             (
                 org_value := tuple(org_value),
-                slope := tuple(goal_elem - org_elem for goal_elem, org_elem in zip(goal_value, org_value)),
+                slope := [goal_elem - org_elem for goal_elem, org_elem in zip(goal_value, org_value)],
             ) if is_seq else (slope := goal_value - org_value),
         ) and (attr_name, org_value, slope, is_seq, )
         for attr_name, goal_value in animated_properties.items()
-    )
+    ]
 
     try:
         clock_event = Clock.schedule_interval(
-            partial(_update, obj, duration, transition, output_seq_type, anim_params, (yield _current_task)[0][0],
-                    [0., ]),
+            partial(_update, obj, duration, transition, anim_params, (yield _current_task)[0][0], [0., ]),
             step,
         )
         yield _sleep_forever
@@ -67,8 +66,7 @@ def _anim_attrs(
         clock_event.cancel()
 
 
-def anim_attrs(obj, *, duration=1.0, step=0, transition=AnimationTransition.linear, output_seq_type=tuple,
-               **animated_properties):
+def anim_attrs(obj, *, duration=1, step=0, transition=AnimationTransition.linear, **animated_properties):
     '''
     Animates attibutes of any object.
 
@@ -78,14 +76,6 @@ def anim_attrs(obj, *, duration=1.0, step=0, transition=AnimationTransition.line
 
         obj = types.SimpleNamespace(x=0, size=(200, 300))
         await anim_attrs(obj, x=100, size=(400, 400))
-
-    The ``output_seq_type`` parameter:
-
-    .. code-block::
-
-        obj = types.SimpleNamespace(size=(200, 300))
-        await anim_attrs(obj, size=(400, 400), output_seq_type=list)
-        assert type(obj.size) is list
 
     .. warning::
 
@@ -99,15 +89,18 @@ def anim_attrs(obj, *, duration=1.0, step=0, transition=AnimationTransition.line
             await anim_attrs(obj, color=(1, 0, 0, 1), pos=(100, 200))  # OK
 
     .. versionadded:: 0.6.1
+    .. versionchanged:: 0.9.0
+        The ``output_seq_type`` parameter was removed.
     '''
-    return _anim_attrs(obj, duration, step, transition, output_seq_type, animated_properties)
+    return _anim_attrs(obj, duration, step, transition, animated_properties)
 
 
-def anim_attrs_abbr(obj, *, d=1.0, s=0, t=AnimationTransition.linear, output_seq_type=tuple,
-                    **animated_properties):
+def anim_attrs_abbr(obj, *, d=1, s=0, t=AnimationTransition.linear, **animated_properties):
     '''
     :func:`anim_attrs` cannot animate attributes named ``step``, ``duration`` and ``transition`` but this one can.
 
     .. versionadded:: 0.6.1
+    .. versionchanged:: 0.9.0
+        The ``output_seq_type`` parameter was removed.
     '''
-    return _anim_attrs(obj, d, s, t, output_seq_type, animated_properties)
+    return _anim_attrs(obj, d, s, t, animated_properties)
