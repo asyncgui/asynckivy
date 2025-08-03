0d9fb332aec543456c4f4934cac05bf43d9f50a7
Nattōsai Mitō <flow4re2c@gmail.com>
2025-08-03 09:15:58 +0900

    Add `free_to_await` parameter to `repeat_sleeping`, also rename it to `sleep_freq`

--- src/asynckivy/_sleep.py
+++ src/asynckivy/_sleep.py
@@ -1,10 +1,10 @@
-__all__ = ("sleep", "sleep_free", "repeat_sleeping", "move_on_after", "n_frames", )
+__all__ = ("sleep", "sleep_free", "repeat_sleeping", "move_on_after", "n_frames", "sleep_freq", )
 
 from contextlib import AbstractAsyncContextManager
 import types
 
 from kivy.clock import Clock
-from asyncgui import _current_task, _sleep_forever, move_on_when, Task, Cancelled
+from asyncgui import _current_task, _sleep_forever, move_on_when, Task, Cancelled, ExclusiveEvent, _wait_args_0
 
 
 @types.coroutine
@@ -47,67 +47,71 @@ def sleep_free(duration):
         raise
 
 
-class repeat_sleeping:
+class sleep_freq:
     '''
-    Returns an async context manager that provides an efficient way to repeat sleeping.
-
-    When there is a piece of code like this:
+    An async form of :meth:`kivy.clock.Clock.schedule_interval`. The following callback-style code:
 
     .. code-block::
 
-        while True:
-            await sleep(0)
-            ...
+        def callback(dt):
+            print(dt)
+            if some_condition:
+                return False
 
-    it can be translated to:
+        Clock.schedule_interval(callback, 0.1)
+
+    is equivalent to the following async-style code:
 
     .. code-block::
 
-        async with repeat_sleeping(step=0) as sleep:
+        async with sleep_freq(0.1) as sleep:
             while True:
-                await sleep()
-                ...
-
-    The latter is more suitable for situations requiring frequent sleeps, such as moving an object in every frame.
-
-    **Restriction**
-
-    You are not allowed to perform any kind of async operations inside the with-block except you can
-    ``await`` the return value of the function that is bound to the identifier of the as-clause.
-
-    .. code-block::
-
-        async with repeat_sleeping(step=0) as sleep:
-            await sleep()  # OK
-            await something_else  # NOT ALLOWED
-            async with async_context_manager:  # NOT ALLOWED
-                ...
-            async for __ in async_iterator:  # NOT ALLOWED
-                ...
+                dt = await sleep()
+                print(dt)
+                if some_condition:
+                    break
 
     .. versionchanged:: 0.8.0
+        The API was made private.
 
-        This API is now private.
+    .. versionchanged:: 0.9.0
+        * The API was made public again.
+        * The API was renamed from ``repeat_sleeping`` to ``sleep_freq``; the old name remains available as an alias.
+        * The ``free_to_await`` parameter was added.
+
+    The ``free_to_await`` parameter
+    --------------------------------
+
+    If set to False (the default), the only permitted async operation within the with-block is ``await xxx()``,
+    where ``xxx`` is the identifier specified in the as-clause. To lift this restriction, set ``free_to_await`` to
+    True—at the cost of slightly reduced performance.
     '''
 
-    __slots__ = ('_step', '_trigger', )
+    __slots__ = ('_step', '_trigger', '_free_to_await')
 
-    @types.coroutine
-    def _sleep(_f=_sleep_forever):
-        return (yield _f)[0][0]
-
-    def __init__(self, *, step=0):
+    def __init__(self, step=0, free_to_await=False):
         self._step = step
+        self._free_to_await = free_to_await
 
     @types.coroutine
-    def __aenter__(self, _sleep=_sleep):
-        task = (yield _current_task)[0][0]
-        self._trigger = Clock.create_trigger(task._step, self._step, True, False)
-        self._trigger()
-        return _sleep
+    def __aenter__(self):
+        if self._free_to_await:
+            e = ExclusiveEvent()
+            self._trigger = t = Clock.create_trigger(e.fire, self._step, True, False)
+            t()
+            return e.wait_args_0
+        else:
+            task = (yield _current_task)[0][0]
+            self._trigger = t = Clock.create_trigger(task._step, self._step, True, False)
+            t()
+            return _wait_args_0
 
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
+    async def __aexit__(self, *args):
         self._trigger.cancel()
+
+
+repeat_sleeping = sleep_freq
+'''An alias of :class:`sleep_freq`.'''
 
 
 def move_on_after(seconds: float) -> AbstractAsyncContextManager[Task]:
