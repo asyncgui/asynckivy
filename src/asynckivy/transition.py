201489ba8f1297c713c551aea4db0d35f8b2de64
Nattōsai Mitō <flow4re2c@gmail.com>
2025-08-06 13:13:23 +0900

    Refactor transition.py

--- src/asynckivy/transition.py
+++ src/asynckivy/transition.py
@@ -16,7 +16,7 @@ from kivy.animation import AnimationTransition
 from kivy.core.window import Window, WindowBase
 from kivy.uix.widget import Widget
 
-from asynckivy import anim_attrs_abbr, transform
+from asynckivy import transform, anim_attrs_abbr as anim_attrs
 
 
 linear = AnimationTransition.linear
@@ -32,9 +32,9 @@ async def fade_transition(target: Wow=Window, *, duration=1, out_curve=linear, i
     canvas = target.canvas
     orig = canvas.opacity
     try:
-        await anim_attrs_abbr(canvas, d=half_d, t=out_curve, opacity=0)
+        await anim_attrs(canvas, d=half_d, t=out_curve, opacity=0)
         yield
-        await anim_attrs_abbr(canvas, d=half_d, t=in_curve, opacity=orig)
+        await anim_attrs(canvas, d=half_d, t=in_curve, opacity=orig)
     finally:
         canvas.opacity = orig
 
@@ -60,11 +60,11 @@ async def slide_transition(target: Wow=Window, *, duration=1, out_curve='in_cubi
     with transform(target, use_outer_canvas=use_outer_canvas) as ig:
         ig.add(mat := Translate())
         half_d = duration / 2
-        await anim_attrs_abbr(mat, d=half_d, t=out_curve, x=x_dist, y=y_dist)
+        await anim_attrs(mat, d=half_d, t=out_curve, x=x_dist, y=y_dist)
         yield
         mat.x = -x_dist
         mat.y = -y_dist
-        await anim_attrs_abbr(mat, d=half_d, t=in_curve, x=0, y=0)
+        await anim_attrs(mat, d=half_d, t=in_curve, x=0, y=0)
 
 
 @asynccontextmanager
@@ -76,9 +76,9 @@ async def scale_transition(target: Wow=Window, *, duration=1, out_curve='out_qua
     with transform(target, use_outer_canvas=use_outer_canvas) as ig:
         ig.add(mat := Scale(origin=target.center))
         half_d = duration / 2
-        await anim_attrs_abbr(mat, d=half_d, t=out_curve, xyz=(0, 0, 1))
+        await anim_attrs(mat, d=half_d, t=out_curve, xyz=(0, 0, 1))
         yield
-        await anim_attrs_abbr(mat, d=half_d, t=in_curve, xyz=(1, 1, 1))
+        await anim_attrs(mat, d=half_d, t=in_curve, xyz=(1, 1, 1))
 
 
 def _calc_enclosing_circle_radius(circle_center, rectangle_size, max=max, hypot=math.hypot, abs=abs):
@@ -130,12 +130,12 @@ async def iris_transition(target: WindowBase=Window, *, duration=1, out_curve='i
 
     canvas.add(ig)
     try:
-        await anim_attrs_abbr(ellipse, d=half_d, t=out_curve, pos=circle_center, size=(0, 0))
+        await anim_attrs(ellipse, d=half_d, t=out_curve, pos=circle_center, size=(0, 0))
         # Setting the ellipse size to (0, 0) isn't enough to nullify the stencil effect for some reason,
         # so we remove it from the canvas and re-add it later.
         inner_ig.remove(ellipse)
         yield
         inner_ig.add(ellipse)
-        await anim_attrs_abbr(ellipse, d=half_d, t=in_curve, pos=ellipse_start_pos, size=ellipse_start_size)
+        await anim_attrs(ellipse, d=half_d, t=in_curve, pos=ellipse_start_pos, size=ellipse_start_size)
     finally:
         canvas.remove(ig)
