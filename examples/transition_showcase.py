9b4af9c6604046f20da46394d28661a77c3afbba
Nattosai Mito <flow4re2c@gmail.com>
2025-10-31 19:11:13 +0900

    Remove `scale_rotate_transition` from `transition_showcase.py`

--- examples/transition_showcase.py
+++ examples/transition_showcase.py
@@ -3,37 +3,15 @@ https://youtu.be/qehA3oIMXZo
 '''
 from textwrap import dedent
 from functools import partial
-from contextlib import asynccontextmanager
 
 from kivy.utils import colormap
-from kivy.graphics import Scale, Rotate, Rectangle
+from kivy.graphics import Rectangle
 from kivy.core.window import Window
 from kivy.app import App
 from kivy.uix.label import Label
 
 import asynckivy as ak
 from asynckivy import transition as t
-
-
-# I didn't include this in the `transition` submodule because the implementation isn't very elegant.
-@asynccontextmanager
-async def scale_rotate_transition(target: t.Wow=Window, *, duration=1, out_curve='out_cubic', in_curve='in_cubic',
-                                  angle=360., use_outer_canvas=False):
-    with ak.transform(target, use_outer_canvas=use_outer_canvas) as ig:
-        center = target.center
-        ig.add(rotate := Rotate(origin=center))
-        ig.add(scale := Scale(origin=center))
-        half_d = duration / 2
-        half_angle = angle / 2
-        await ak.wait_all(
-            ak.anim_attrs_abbr(scale, d=half_d, t=out_curve, xyz=(0, 0, 1)),
-            ak.anim_attrs_abbr(rotate, d=half_d, angle=half_angle),
-        )
-        yield
-        await ak.wait_all(
-            ak.anim_attrs_abbr(scale, d=half_d, t=in_curve, xyz=(1, 1, 1)),
-            ak.anim_attrs_abbr(rotate, d=half_d, angle=angle),
-        )
 
 
 class TestApp(App):
@@ -89,10 +67,6 @@ class TestApp(App):
 
             await touch_down()
             async with t.scale_transition(duration=0.4):
-                label.text = 'scale_rotate_transition'
-
-            await touch_down()
-            async with scale_rotate_transition(duration=0.6):
                 label.text = 'fade_transition'
 
             await touch_down()
